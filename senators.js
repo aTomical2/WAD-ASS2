bed5d171f20b9fc0e420f1759d7d3fa6dd05ebe1
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-10-18 14:59:00 +0100

    changes

--- senators.js
+++ senators.js
@@ -19,67 +19,97 @@ async function getSenatorData() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-  let rawData = await getSenatorData();
-  let senator = rawData.objects;
+let data = getSenatorData()
+  .then(x => {
+    data = x;
+  });
 
-  senatorClassArray = []
-  for (let i=0; i < rawData.meta.limit; i++) {
-    let senName = new Senator(
-      senator[i].caucus,
-      senator[i].congress_number,
-      senator[i].current,
-      senator[i].description,
-      senator[i].district,
-      senator[i].enddate,
-      senator[i].extra,
-      senator[i].leadership_title,
-      senator[i].party,
-      senator[i].person,
-      senator[i].phone,
-      senator[i].role_type,
-      senator[i].role_type_label,
-      senator[i].senator_class,
-      senator[i].senator_class_label,
-      senator[i].senator_rank,
-      senator[i].senator_rank_label,
-      senator[i].state,
-      senator[i].title,
-      senator[i].title_long,
-      senator[i].website,
-      )
-    senatorClassArray.push(senName)
-}
-console.log(senatorClassArray)
-});
+
+let button = document.createElement('button')
+button.innerText = 'PLEASE WORK'
+button.addEventListener('click', () => {
+  console.log(data)})
+document.querySelector('body').append(button)
 
 
 
-class Senator{
-  constructor(caucus,congress_number,current,description,district,enddate,extra,leadership_title,party,person,phone,role_type,role_type_label,senator_class,senator_class_label,senator_rank,senator_rank_label,state,title,title_long,website) {
-    this.caucus = caucus
-    this.congress_number = congress_number
-    this.current = current
-    this.description = description
-    this.district = district
-    this.enddate = enddate
-    this.extra = extra
-    this.leadership_title = leadership_title
-    this.party = party
-    this.person = person
-    this.phone = phone
-    this.role_type = role_type
-    this.role_type_label = role_type_label
-    this.senator_class = senator_class
-    this.senator_class_label = senator_class_label
-    this.senator_rank = senator_rank
-    this.senator_rank_label = senator_rank_label
-    this.state = state
-    this.title = title
-    this.title_long = title_long
-    this.website = website
-  }
-}
+
+
+
+
+
+
+
+
+
+
+  // .then(x => {
+  //   let body = document.querySelector("body")
+  //   body.innerText = JSON.stringify(x)
+  // })
+// document.addEventListener("DOMContentLoaded", async () => {
+//   rawData = await getSenatorData();
+//   senator = rawData.objects;
+
+//   // for (let i=0; i < rawData.meta.limit; i++) {
+//     let senName = new Senator(
+//       senator[i].caucus,
+//       senator[i].congress_number,
+//       senator[i].current,
+//       senator[i].description,
+//       senator[i].district,
+//       senator[i].enddate,
+//       senator[i].extra,
+//       senator[i].leadership_title,
+//       senator[i].party,
+//       senator[i].person,
+//       senator[i].phone,
+//       senator[i].role_type,
+//       senator[i].role_type_label,
+//       senator[i].senator_class,
+//       senator[i].senator_class_label,
+//       senator[i].senator_rank,
+//       senator[i].senator_rank_label,
+//       senator[i].state,
+//       senator[i].title,
+//       senator[i].title_long,
+//       senator[i].website,
+//       )
+//     senatorClassArray.push(senName)
+// }
+
+
+
+
+
+
+
+
+// class Senator{
+//   constructor(caucus,congress_number,current,description,district,enddate,extra,leadership_title,party,person,phone,role_type,role_type_label,senator_class,senator_class_label,senator_rank,senator_rank_label,state,title,title_long,website) {
+//     this.caucus = caucus
+//     this.congress_number = congress_number
+//     this.current = current
+//     this.description = description
+//     this.district = district
+//     this.enddate = enddate
+//     this.extra = extra
+//     this.leadership_title = leadership_title
+//     this.party = party
+//     this.person = person
+//     this.phone = phone
+//     this.role_type = role_type
+//     this.role_type_label = role_type_label
+//     this.senator_class = senator_class
+//     this.senator_class_label = senator_class_label
+//     this.senator_rank = senator_rank
+//     this.senator_rank_label = senator_rank_label
+//     this.state = state
+//     this.title = title
+//     this.title_long = title_long
+//     this.website = website
+//   }
+// }
 
 
 
