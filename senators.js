ee29d4a8ebcfc5a29591d3ff6b68bbc47882ac83
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2023-10-18 15:58:35 +0100

    Added function to get data

--- senators.js
+++ senators.js
@@ -1,25 +1,32 @@
-
 // function to assemble data from the JSON file
 async function getSenatorData() {
-    try {
-        const url = "senators.json";
-        const promise = await fetch(url);
+  try {
+    // const url = "http://127.0.0.1:5500/senators.json";
+    const url = "/senators.json";
+    const promise = await fetch(url);
 
-        // Check if the request was successful
-        if (!promise.ok) {
-            throw new Error(
-                `HTTP error! status: ${promise.status}`
-            );
-        }
-
-        const data = await promise.json();
-
-        displayJSON(data);
-    } catch (error) {
-        document.getElementById("id01").innerText = error;
+    // Check if the request was successful
+    if (!promise.ok) {
+      throw new Error(`HTTP error! status: ${promise.status}`);
     }
+
+    const data = await promise.json();
+    return data;
+
+    // return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+let data = getSenatorData()
+  .then(x => {
+    data = x;
+  });
 
-let data = getSenatorData();
-console.log(data);
+
+let button = document.createElement('button')
+button.innerText = 'PLEASE WORK'
+button.addEventListener('click', () => {
+  console.log(data)})
+document.querySelector('body').append(button)
